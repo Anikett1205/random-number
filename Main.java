9d3dab4a0930df78a9d0cd07f7a58abdbf8aac25
Aniket <bagadeaniket1111@gmail.com>
2023-04-05 16:02:49 +0530

    Add random number generator

---
+++ Main.java
@@ -0,0 +1,11 @@
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+        Random rand = new Random();
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println(rand.nextInt(100));
+        }
+    }
+}
